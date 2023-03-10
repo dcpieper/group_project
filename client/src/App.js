0e3928a6ef516c7198b8f37ff172841717c81f5b
gramscott <120787031+gramscott@users.noreply.github.com>
2023-03-10 13:37:40 +0000

    App.js rough draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <h1>smeco</h1>
+  <nav>
+    <ul>
+       <li><a href="/home">Home</a></li>
+       <li><a href="/quiz">Quiz</a></li>
+       <li><a href="/community">Community</a></li>
+       <li><a href="/leaderboard">Leaderboard</a></li>
+    </ul>
+  </nav>
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1200px-Grosser_Panda.JPG"/>
+  
+
+  </>
+  )
 }
 
 export default App;

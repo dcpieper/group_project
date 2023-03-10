70ed17cb7c6554392a971cf141f5e4079e5aa4d5
gramscott <120787031+gramscott@users.noreply.github.com>
2023-03-10 14:59:32 +0000

    front-end work quiz page

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomepageContainer from './containers /HomepageContainer';
+import NavBar from './components /NavBar';
 
 function App() {
 
   return (
-  <>
-  <h1>smeco</h1>
-  <nav>
-    <ul>
-       <li><a href="/home">Home</a></li>
+  <Router >
+  <header>
+    <h1>smeco</h1>
+  </header>
+  
+
+  <HomepageContainer/>
+
+  <footer>
+    <nav>
+      <ul>
+        <li><a href="/home">Home</a></li>
        <li><a href="/quiz">Quiz</a></li>
        <li><a href="/community">Community</a></li>
        <li><a href="/leaderboard">Leaderboard</a></li>
-    </ul>
-  </nav>
-  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1200px-Grosser_Panda.JPG"/>
-  
+      </ul>
+      </nav>
+      <h4>smeco ©</h4>
+  </footer>
+
+
 
   </>
   )
---
+++ client/src/containers /HomepageContainer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const HomepageContainer = () => {
+
+  return (
+    <>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Grosser_Panda.JPG/1200px-Grosser_Panda.JPG"/>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur ligula ac tortor vestibulum, in fringilla ligula aliquet. Sed quis pretium enim, faucibus fringilla dui. Nam maximus diam nec metus imperdiet, at varius libero lobortis. In congue mattis hendrerit. Sed auctor, ligula sit amet sagittis convallis, purus nunc sollicitudin nisi, nec semper odio nulla at sapien. Vestibulum sagittis tortor ac quam aliquet vulputate a id quam. Curabitur lacinia, libero ut volutpat rutrum, lacus mauris varius sapien, eu venenatis enim nunc luctus purus. Cras ligula augue, ultricies quis erat eget, accumsan faucibus erat. Nulla posuere mattis lacinia.</p>
+    
+    <button type="submit">Start Quiz!</button>
+    </>
+  )
+
+}
+
+export default HomepageContainer

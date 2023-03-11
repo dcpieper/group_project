c7304099127af14eff4e068644ec27ac89468fdb
Phil Broadley <philbrocoding@hotmail.com>
2023-03-11 10:46:06 +0000

    quiz-functionality

--- client/src/components /QuizForm.js
+++ client/src/components /QuizForm.js
@@ -1,145 +1,283 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-export const QuizForm = () => {
-   
-        const [name, setName] = useState(null);
-        const [score, setScore ] = useState(null);
-        const [formComplete, setFormComplete] = useState(false);
-        const [country, setCountry] = useState(null);
+const QuizForm = ({ getQuizResult }) => {
+  const [name, setName] = useState(null);
+  const [country, setCountry] = useState(null);
+  const [detailsFormComplete, setDetailsFormComplete] = useState(false);
+  const [questionOneScore, setQuestionOneScore] = useState(0);
+  const [questionTwoScore, setQuestionTwoScore] = useState(0);
+  const [questionThreeScore, setQuestionThreeScore] = useState(0);
+  const [questionFourScore, setQuestionFourScore] = useState(0);
+  const [questionFiveScore, setQuestionFiveScore] = useState(0);
+  const [totalScore, setTotalScore] = useState(0);
+  const [quizFormComplete, setQuizFormComplete] = useState(false);
 
-        const userName = (event) => {
-          setName(event.target.value);
-        }
-        const userCountry = (event) => {
-          setCountry(event.target.value);
-        }
+  const handleUserNameChange = (event) => {
+    setName(event.target.value);
+  };
+  const handleUserCountryChange = (event) => {
+    setCountry(event.target.value);
+  };
 
-        const userDetails = (event) => {
-          event.preventDefault();
-          setFormComplete(true);
-        }
+  const handleUserDetailsSubmit = (event) => {
+    event.preventDefault();
+    setDetailsFormComplete(true);
+  };
 
+  const handleQuestionOne = (event) => {
+    setQuestionOneScore(parseInt(event.target.value));
+  };
+  const handleQuestionTwo = (event) => {
+    setQuestionTwoScore(parseInt(event.target.value));
+  };
+  const handleQuestionThree = (event) => {
+    setQuestionThreeScore(parseInt(event.target.value));
+  };
+  const handleQuestionFour = (event) => {
+    setQuestionFourScore(parseInt(event.target.value));
+  };
+  const handleQuestionFive = (event) => {
+    setQuestionFiveScore(parseInt(event.target.value));
+  };
+
+  const handleQuizFormSubmit = (event) => {
+    event.preventDefault();
+    setTotalScore(
+      questionOneScore +
+        questionTwoScore +
+        questionThreeScore +
+        questionFourScore +
+        questionFiveScore
+    );
+    setQuizFormComplete(true);
+  };
+  getQuizResult(totalScore);
+  // console.log(questionOneScore);
+  // console.log(questionTwoScore);
+  // console.log(questionThreeScore);
+  // console.log(questionFourScore);
+  // console.log(questionFiveScore);
+  // console.log(totalScore);
 
   return (
+    <div>
+      {detailsFormComplete ? (
+        <h2>
+          Hello {name} from {country}. Please begin the quiz below.
+        </h2>
+      ) : (
+        <form onSubmit={handleUserDetailsSubmit}>
+          <h4>Please enter your details below to begin the quiz:</h4>
+          <input
+            onChange={handleUserNameChange}
+            type="text"
+            placeholder="name"
+            name="name"
+          />
+          <input
+            onChange={handleUserCountryChange}
+            type="text"
+            placeholder="country"
+            name="country"
+          />
+          <input type="submit" />
+        </form>
+      )}
 
-<div>
-{ formComplete ? 
-<h2>Hello {name} from {country}</h2> : 
-        <form onSubmit={userDetails}>
-<h4>Please enter your details below:</h4>
-<input onChange={userName} type='text' placeholder='name' name='name'/>
-<input onChange={userCountry} type='text' placeholder='country' name='country' />
-<input type='submit'/>
-        </form> 
-}
+      {!quizFormComplete && detailsFormComplete ? (
+        <form onSubmit={handleQuizFormSubmit}>
+          <div>
+            <h4>What type of transport do you use?</h4>
+            <label htmlFor="walk">Walk</label>
+            <input
+              type="radio"
+              name="question1"
+              htmlFor="walk"
+              value={1}
+              onChange={handleQuestionOne}
+              required
+            />
 
+            <label htmlFor="walk">Train</label>
+            <input
+              type="radio"
+              name="question1"
+              htmlFor="train"
+              value={2}
+              onChange={handleQuestionOne}
+            />
 
+            <label htmlFor="walk">Bus</label>
+            <input
+              type="radio"
+              name="question1"
+              htmlFor="bus"
+              value={3}
+              onChange={handleQuestionOne}
+            />
 
-   <form>
+            <label htmlFor="walk">Car</label>
+            <input
+              type="radio"
+              name="question1"
+              htmlFor="car"
+              value={4}
+              onChange={handleQuestionOne}
+            />
 
-      <div>
-        <h4>What type of transport do you use?</h4>
-        <label htmlFor="walk">Walk</label>
-        <input type="radio" name="question1" for="walk" />
+            <label htmlFor="walk">Plane</label>
+            <input
+              type="radio"
+              name="question1"
+              htmlFor="plane"
+              value={5}
+              onChange={handleQuestionOne}
+            />
+          </div>
 
-        <label htmlFor="walk">Train</label>
-        <input type="radio" name="question1" for="train" />
+          <div>
+            <h4>How much meat do you eat?</h4>
+            <label htmlFor="never">Never eat meat or dairy</label>
+            <input
+              type="radio"
+              name="question2"
+              htmlFor="never"
+              value={1}
+              onChange={handleQuestionTwo}
+              required
+            />
 
-        <label htmlFor="walk">Bus</label>
-        <input type="radio" name="question1" for="bus" />
+            <label htmlFor="nevermeat">Never eat meat</label>
+            <input
+              type="radio"
+              name="question2"
+              htmlFor="nevermeat"
+              value={2}
+              onChange={handleQuestionTwo}
+            />
 
-        <label htmlFor="walk">Car</label>
-        <input type="radio" name="question1" for="car" />
+            <label htmlFor="sometimes">I sometimes eat meat</label>
+            <input
+              type="radio"
+              name="question2"
+              htmlFor="sometimes"
+              value={3}
+              onChange={handleQuestionTwo}
+            />
 
-        <label htmlFor="walk">Plane</label>
-        <input type="radio" name="question1" for="plane" />
-      </div>
+            <label htmlFor="mostly">I mostly eat meals during meals</label>
+            <input
+              type="radio"
+              name="question2"
+              htmlFor="mostly"
+              value={4}
+              onChange={handleQuestionTwo}
+            />
 
+            <label htmlFor="every">I eat meat during every meal</label>
+            <input
+              type="radio"
+              name="question2"
+              htmlFor="every"
+              value={5}
+              onChange={handleQuestionTwo}
+            />
+          </div>
 
+          <div>
+            <h4>What do you recycle?</h4>
+            <label htmlFor="glass">Glass</label>
+            <input type="checkbox" name="question3" htmlFor="glass" />
 
-      <div>        
-        <h4>How much meat do you eat?</h4>
-        <label htmlFor="never">Never eat meat or dairy</label>
-        <input type="radio" name="question2" for="never" />
+            <label htmlFor="paper">Paper/Cardboard</label>
+            <input type="checkbox" name="question3" htmlFor="paper" />
 
-        <label htmlFor="nevermeat">Never eat meat</label>
-        <input type="radio" name="question2" for="nevermeat" />
+            <label htmlFor="food">Food</label>
+            <input type="checkbox" name="question3" htmlFor="food" />
 
-        <label htmlFor="sometimes">I sometimes eat meat</label>
-        <input type="radio" name="question2" for="sometimes" />
+            <label htmlFor="tins">Tins/Cans</label>
+            <input type="checkbox" name="question3" htmlFor="tins" />
 
-        <label htmlFor="mostly">I mostly eat meals during meals</label>
-        <input type="radio" name="question2" for="mostly" />
+            <label htmlFor="plastics">Plastics</label>
+            <input type="checkbox" name="question3" htmlFor="plastics" />
+          </div>
 
-        <label htmlFor="every">I eat meat during every meal</label>
-        <input type="radio" name="question2" for="every" />
-        </div>
+          <div>
+            <h4>How much on average do you spend on electronics per month?</h4>
+            <label htmlFor="zero">£0-£25</label>
+            <input
+              type="radio"
+              name="question4"
+              htmlFor="zero"
+              value={1}
+              onChange={handleQuestionFour}
+              required
+            />
 
+            <label htmlFor="twenty">£26-£100</label>
+            <input
+              type="radio"
+              name="question4"
+              htmlFor="twenty"
+              value={2}
+              onChange={handleQuestionFour}
+            />
 
+            <label htmlFor="hundred">£101-£250</label>
+            <input
+              type="radio"
+              name="question4"
+              htmlFor="hundred"
+              value={3}
+              onChange={handleQuestionFour}
+            />
 
-      <div>   
-             <h4>What do you recycle?</h4>
-        <label htmlFor="glass">Glass</label>
-        <input type="checkbox" name="question3" for="glass" />
+            <label htmlFor="loads">£251-£500</label>
+            <input
+              type="radio"
+              name="question4"
+              htmlFor="loads"
+              value={4}
+              onChange={handleQuestionFour}
+            />
 
-        <label htmlFor="paper">Paper/Cardboard</label>
-        <input type="checkbox" name="question3" for="paper" />
+            <label htmlFor="most">£501+</label>
+            <input
+              type="radio"
+              name="question4"
+              htmlFor="most"
+              value={5}
+              onChange={handleQuestionFour}
+            />
+          </div>
 
-        <label htmlFor="food">Food</label>
-        <input type="checkbox" name="question3" for="food" />
+          <div>
+            <h4>What do you have in your home?</h4>
+            <label htmlFor="double">Double glazing</label>
+            <input type="checkbox" name="question5" htmlFor="double" />
 
-        <label htmlFor="tins">Tins/Cans</label>
-        <input type="checkbox" name="question3" for="tins" />
+            <label htmlFor="lightbulbs">Energy saving light bulbs</label>
+            <input type="checkbox" name="question5" htmlFor="lightbulbs" />
 
-        <label htmlFor="plastics">Plastics</label>
-        <input type="checkbox" name="question3" for="plastics" />
-        </div>
+            <label htmlFor="loft">Loft insulation</label>
+            <input type="checkbox" name="question5" htmlFor="loft" />
 
+            <label htmlFor="solar">Solar panels</label>
+            <input type="checkbox" name="question5" htmlFor="solar" />
 
+            <label htmlFor="boiler">Condensing boiler</label>
+            <input type="checkbox" name="question5" htmlFor="boiler" />
+          </div>
 
-      <div>   
-             <h4>How much on average do you spend on electronics per month?</h4>
-        <label htmlFor="zero">£0-£25</label>
-        <input type="radio" name="question4" for="zero" />
-
-        <label htmlFor="twenty">£26-£100</label>
-        <input type="radio" name="question4" for="twenty" />
-
-        <label htmlFor="hundred">£101-£250</label>
-        <input type="radio" name="question4" for="hundred" />
-
-        <label htmlFor="loads">£251-£500</label>
-        <input type="radio" name="question4" for="loads" />
-
-        <label htmlFor="most">£501+</label>
-        <input type="radio" name="question4" for="most" />
-        </div>
-
-
-      <div>   
-             <h4>What do you have in your home?</h4>
-        <label htmlFor="double">Double glazing</label>
-        <input type="checkbox" name="question5" for="double" />
-
-        <label htmlFor="lightbulbs">Energy saving light bulbs</label>
-        <input type="checkbox" name="question5" for="lightbulbs" />
-
-        <label htmlFor="loft">Loft insulation</label>
-        <input type="checkbox" name="question5" for="loft" />
-
-        <label htmlFor="solar">Solar panels</label>
-        <input type="checkbox" name="question5" for="solar" />
-
-        <label htmlFor="boiler">Condensing boiler</label>
-        <input type="checkbox" name="question5" for="boiler" />
-        </div>
-
-
-      <div>
-        <input type="submit"></input>
-      </div>
-    </form>
+          <div>
+            <input type="submit"></input>
+          </div>
+        </form>
+      ) : null}
+      {quizFormComplete ? (
+        <h2>Thank you for taking the quiz. Here are your results:</h2>
+      ) : null}
     </div>
-  )
-}
+  );
+};
 export default QuizForm;
--- client/src/containers /QuizContainer.js
+++ client/src/containers /QuizContainer.js
@@ -1,11 +1,25 @@
-import React from "react";
-import { QuizForm } from "../components /QuizForm";
+import { useState, useEffect } from "react";
+import QuizForm from "../components /QuizForm";
 
 const QuizContainer = () => {
+  const [quizResult, setQuizResult] = useState(0);
 
- 
+  const getQuizResult = (result) => {
+    setQuizResult(result);
+  };
+
+  console.log(quizResult);
+
   return (
-    <QuizForm />
+    <div>
+      <QuizForm getQuizResult={getQuizResult} />
+      {quizResult ? (
+        <h2>
+          Your result is {quizResult}. This is similair to COUNTRY INFO
+          COMPONENT.
+        </h2>
+      ) : null}
+    </div>
   );
 };
 

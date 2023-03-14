31a822f3a868bf0dbe35be307aaace8479c48651
Phil Broadley <philbrocoding@hotmail.com>
2023-03-14 13:47:22 +0000

    notices in date order

--- client/src/components /Notice.js
+++ client/src/components /Notice.js
@@ -4,12 +4,20 @@ const Notice = ({ notice }) => {
   const noticeDelete = () => {
     deleteNotice(notice._id);
   };
+  const splicedDate = notice.created.split("T")[0];
+  const splitDate = splicedDate.split("-");
+  const day = splitDate[2];
+  const month = splitDate[1];
+  const year = splitDate[0];
+
   return (
     <div>
       <h3>{notice.name}</h3>
       <p>{notice.message}</p>
       <p>{notice.category}</p>
-      <p>{notice.created}</p>
+      <p>
+        {day}-{month}-{year}
+      </p>
       <form onSubmit={noticeDelete}>
         <button>Delete</button>
       </form>

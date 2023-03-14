416d1fd1731c3578572d28e0178b974ed5cafb80
Phil Broadley <philbrocoding@hotmail.com>
2023-03-14 15:51:09 +0000

    fix

--- client/src/containers /HomepageContainer.js
+++ client/src/containers /HomepageContainer.js
@@ -9,7 +9,7 @@ const HomepageContainer = () => {
       Are you sick of hearing or speaking about the world ending? Do you want to help but don’t know where to start?
       <br/>
       We can help you with some simple tips and guidance. First take our short quiz to get you started. Then join our personalised Community page. You’ll receive up to date education and can become an integral part of saving our planet!
-      <br>
+      <br/>
       What are you waiting for?</p>
     <h2><a href='/quiz'>Start Quiz </a></h2>
     </div>

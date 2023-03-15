28b52441e6cc6a01f7ba9e9dc014e44d5b8dc626
Phil Broadley <philbrocoding@hotmail.com>
2023-03-15 09:37:56 +0000

    added title and favicon

--- client/src/components /QuizForm.js
+++ client/src/components /QuizForm.js
@@ -22,7 +22,7 @@ const QuizForm = ({ getQuizResult, findFact }) => {
   const [loft, setLoft] = useState(false);
   const [solar, setSolar] = useState(false);
   const [boiler, setBoiler] = useState(false);
-  const [formData, setFormData] = useState({git 
+  const [formData, setFormData] = useState({
     name: "",
     score: 0,
     country: "",
